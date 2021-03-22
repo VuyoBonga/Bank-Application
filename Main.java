d0a32a380932486a83067185b2f6bf7aba6c1eb6
Vuyo Bonga <bongav.vb@gmail.com>
2021-03-22 10:38:37 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Account account = new Account("Vuyo", "970527");
+        account.showMenu();
+
+
+    }
+
+
+}
